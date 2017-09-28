35cdb42e9415d0474e948819f1ff93887d8ef67c
hliuece2016 <hliuece2016@gmail.com>
2017-09-28 13:02:12 -0400

    test

--- src/servlet/teacherServlet.java
+++ src/servlet/teacherServlet.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import daoFactory.DaoFactory;
-import daoImpl.TeacherImpl;
-import dbc.DatabaseConnection;
 import vo.studentRecord;
 
 /**
