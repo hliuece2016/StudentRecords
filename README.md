af0d344e87a294be796b277638658c97ccfe77bc
hliuece2016 <hliuece2016@gmail.com>
2017-10-02 22:45:32 -0400

    add readme and database export file.

---
+++ README.md
@@ -0,0 +1,5 @@
+# StudentRecordsSystem
+This system allows students to login into their account and search their records, and register new student account.
+And this system allows teacher to login into their account and search, update student records. And show all students records. 
+For now, there is only one teacher account in system.
+Welcome to use this system!
---
+++ student.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: student
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `student_tbl`
+--
+
+DROP TABLE IF EXISTS `student_tbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student_tbl` (
+  `ID` decimal(2,0) DEFAULT NULL,
+  `NAME` varchar(40) NOT NULL,
+  `HOMEWORK` decimal(3,0) NOT NULL,
+  `MIDTERM` decimal(3,0) NOT NULL,
+  `FINAL` decimal(3,0) NOT NULL,
+  `PASSWORD` varchar(20) NOT NULL,
+  PRIMARY KEY (`NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_tbl`
+--
+
+LOCK TABLES `student_tbl` WRITE;
+/*!40000 ALTER TABLE `student_tbl` DISABLE KEYS */;
+INSERT INTO `student_tbl` VALUES (3,'ann',11,22,33,'0000'),(1,'bob',10,2,3,'1111'),(6,'hao',1,2,3,'0000'),(0,'jack',0,0,0,'0000'),(0,'mark',0,0,0,'1234'),(2,'peter',10,20,30,'0000'),(0,'teacher',1,2,3,'teacher');
+/*!40000 ALTER TABLE `student_tbl` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-02 22:41:42
